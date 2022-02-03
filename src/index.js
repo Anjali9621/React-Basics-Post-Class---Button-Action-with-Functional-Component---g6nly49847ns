472826b31debc27ee658e427e1fb2ae4ad1ec19b
Anjali9621 <95613922+Anjali9621@users.noreply.github.com>
2022-02-03 12:51:40 +0530

    str

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, 
+  <button id="click"></button>
+  <p id="para">
+    Hello, I've learnt to use the full-stack evaluation tool. This makes me
+    so happy
+  </p>
+  document.getElementById("root"));
