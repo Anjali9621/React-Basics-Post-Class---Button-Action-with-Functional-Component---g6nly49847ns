7ad861de1590565f97db238cb0d0a9dbc3516278
Anjali9621 <95613922+Anjali9621@users.noreply.github.com>
2022-02-09 22:13:28 +0530

    str

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,15 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [isClicked, setClick] = useState(false);
   return (
     <div id="main">
-      <button id="click">
+      <button id="click" onClick={() => setClick(true>  </button>
         <p id="para">
           Hello, I've learnt to use the full-stack evaluation tool. This makes
           me so happy
         </p>
-      </button>
+        <span />
     </div>
   );
 }
