19712c7d60980f77a2c061bdb4ca053c871f28dd
Anjali9621 <95613922+Anjali9621@users.noreply.github.com>
2022-02-15 22:49:29 +0530

    button5

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,19 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [isClicked, setClick] = useState(false);
+  const [data, setdata] = useState(false);
   return (
     <div id="main">
-      <button id="click" onClick={() => setstate(true)}>
-        {state ? (
-          <p id="para">
-            Hello, I've learnt to use the full-stack evaluation tool. This makes
-            me so happy
-          </p>
-        ) : (
-          "!"
-        )}
+      <button id="click" onClick={() => setdata(true)}>
+        click
       </button>
+      {data && (
+        <p id="para">
+          {" "}
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
     </div>
   );
 }
